94476d621ab0fb930ca1777738b6ce9ea9933976
Gihong Kim <gyong1211@gmail.com>
2019-01-28 15:48:21 +0900

    Modify test method name

--- test/test_many_flavor_of_mock_patch.py
+++ test/test_many_flavor_of_mock_patch.py
@@ -6,26 +6,26 @@ from helpers import BaseHelper
 
 @mock.patch('helpers.get_response')
 class UseMockPatchAsClassDecorator(TestCase):
-    def test_클래스데코레이터로_사용(self, mocked_response):
-        mocked_response.return_value.json.return_value = {"result": True}
-        mocked_response.return_value.status_code = 200
+    def test_get_response(self, mocked_get_response):
+        mocked_get_response.return_value.json.return_value = {"result": True}
+        mocked_get_response.return_value.status_code = 200
         result = BaseHelper.get_site_status("http://non-existent-url")
         assert result == {"code": 200}
 
 
 class UseMockPatchAsMethodDecorator(TestCase):
     @mock.patch('helpers.get_response')
-    def test_메서드데코레이터로_사용(self, mocked_response):
-        mocked_response.return_value.json.return_value = {"result": True}
-        mocked_response.return_value.status_code = 200
+    def test_get_response(self, mocked_get_response):
+        mocked_get_response.return_value.json.return_value = {"result": True}
+        mocked_get_response.return_value.status_code = 200
         result = BaseHelper.get_site_status("http://non-existent-url")
         assert result == {"code": 200}
 
 
 class UseMockPatchAsContextManager(TestCase):
-    def test_컨텍스트매니저로_사용(self):
-        mocked_response = Mock(json=Mock(return_value={"result": True}), status_code=200)
-        with mock.patch('helpers.get_response', return_value=mocked_response):
+    def test_get_response(self):
+        with mock.patch('helpers.get_response') as mocked_get_response:
+            mocked_get_response.return_value = Mock(json=Mock(return_value={"result": True}), status_code=200)
             result = BaseHelper.get_site_status("http://non-existent-url")
             assert result == {"code": 200}
         result = BaseHelper.get_site_status("http://non-existent-url")
@@ -33,10 +33,10 @@ class UseMockPatchAsContextManager(TestCase):
 
 
 class TestUseMockPatchManually(TestCase):
-    def test_수동으로_patch(self):
+    def test_get_response(self):
         patch_get_response = mock.patch('helpers.get_response')
-        mocked_response = patch_get_response.start()  # get_response patch 시작
-        mocked_response.return_value = Mock(json=Mock(return_value={"result": True}), status_code=200)
+        mocked_get_response = patch_get_response.start()  # get_response patch 시작
+        mocked_get_response.return_value = Mock(json=Mock(return_value={"result": True}), status_code=200)
         result = BaseHelper.get_site_status("http://non-existent-url")
         assert result == {"code": 200}
         patch_get_response.stop()  # get_response patch 종료
