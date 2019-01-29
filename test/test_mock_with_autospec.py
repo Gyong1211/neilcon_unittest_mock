d14c2a13f61f9877a1dff306f22b5b0fc8f51354
Gihong Kim <gyong1211@gmail.com>
2019-01-29 10:10:23 +0900

    Add test_mock_with_autospec test function 추가

--- test/test_mock_with_autospec.py
+++ test/test_mock_with_autospec.py
@@ -9,3 +9,11 @@ def test_get_site_status_without_autospec(mocked_get_response):
     mocked_get_response.return_value.status_code = 200
     result = BaseHelper.get_site_status("http://non-existent-url")
     assert result == {"code": 200}
+
+
+@mock.patch('helpers.get_response', autospec=True)
+def test_get_site_status_with_autospec(mocked_get_response):
+    mocked_get_response.return_value.json.return_value = {"result": True}
+    mocked_get_response.return_value.status_code = 200
+    result = BaseHelper.get_site_status("http://non-existent-url")
+    assert result == {"code": 200}
